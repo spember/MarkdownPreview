d2ad00ea98408e233c5ed7690026c94c7855313b
Steve Pember <steve@cantinaconsulting.com>
2012-01-23 19:34:28 -0500

    -Rolled back previous change -Added instructions to the Readme for special utf characters

--- MarkdownPreview.py
+++ MarkdownPreview.py
@@ -7,58 +7,9 @@ class MarkdownPreviewCommand(sublime_plugin.TextCommand):
 
 
 
-	def convert(self, orig_file_name, view, format="html"):
-
-		has_html = self.has_html_header( view )
-		if( has_html ):
-			file_name = orig_file_name
-		else:
-			file_name = self.wrap_content_in_temp_file(orig_file_name)
-		
-		temp_html_name = file_name[0:file_name.rfind(".")]+".html"
-		final_html_name = orig_file_name[0:orig_file_name.rfind(".")] +".html"
+	def convert(self, file_name, format="html"):
 		settings = sublime.load_settings(__name__ + '.sublime-settings')
-		subprocess.call([settings.get("mmd_path"), "--to="+str(format), file_name])
-
-		if(not has_html):			
-			os.rename(temp_html_name, final_html_name)
-			#aaaand cleanup the temp file
-			#careful...
-			os.remove(file_name)
-
-
-		
-
-
-	def wrap_content_in_temp_file(self, orig_file_name):
-		"""In case the buffer does not contain an opening html tag, will populate a temp file
-		"""
-		
-		current_path = orig_file_name[0:orig_file_name.rfind("/")]
-		tmp_file_name = current_path+"/MMDTmp.md"
-
-		#create temporary file, wrap the contents in it, and save. Then return the name
-		tmp_file = open(tmp_file_name, 'w')
-		original = open(orig_file_name, 'r')
-
-		tmp_file.write(self.header)
-		tmp_file.write(original.read())
-		tmp_file.write(self.footer)
-		tmp_file.close()
-		original.close()
-
-
-		return tmp_file_name
-
-		
-
-	def has_html_header(self, view ):
-		"""Returns true if the first 50 characters contains an opening html node ('<html>')"""
-		text = view.substr(sublime.Region(0,50))
-		found = False
-		if(text.find("<html>") > -1):
-			found = True
-		return found
+		subprocess.call([settings.get("mmd_path"), "--to="+str(format), file_name ])
 
 	def open_in_browser(self, base_file_name):
 		url = "file://"+str(base_file_name)
@@ -68,20 +19,9 @@ class MarkdownPreviewCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		"""Currently only converts to HTML"""
 		file_name = self.view.file_name()
-		self.convert( file_name, self.view )
+		self.convert( file_name )
 		self.open_in_browser( file_name )
 
-
-	header = """<html>
-		<head>
-			<title>MultiMarkdown Preview</title>
-			<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		</head>
-		<body>
-	"""
-	
-	footer = """</body>
-	</html>"""
 
 
 		
--- README.markdown
+++ README.markdown
@@ -1,3 +1,8 @@
+<head>
+	<title>MarkdownPreview Readme</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
+
 # MarkdownPreview #
 ### A MultiMarkdown package for [SublimeText 2][subl] ###
 
@@ -18,6 +23,24 @@ So, I downloaded [MultiMarkdown][mmd] and threw this quickly together. I've been
 
 ## Installation ##
 Clone this repo into your Packages folder. To execute the preview, you can use the command palette to execute the preview (Markdown Preview: HTML) or the default key binding of cmd-shift-m.
+
+If your MultiMarkdown path is not '/usr/local/bin/mmd', adjust the path for the 'mmd_path' variable in your Sublime configuration.
+
+## Usage ##
+To generate a preview, view your saved markdown file (currently a valid saved file is required to pass into Multi Markdown) and press cmd+M (cmd+shift+m). A converted html version of your markdown file should appear in your default browser. These keystrokes can be updated/set in the keybindings.
+
+Alternatively, execute the "Markdown Preview:HTML" command from the Command Palette.
+
+## UTF-8 Special Characters ##
+
+In order for this module to process UTF-8 characters (e.g. 'ö' or 'è') correctly, you should include a meta tag deonoting that the document is UTF-8. At a minimum, this will look like:
+
+	<head>
+		<title>Some Title</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	</head>
+
+With the above included in your document, 
 
 ## Notes ##
 Since this was developed for Mac OS X, there's been no testing for windows / linux. The default key binding is also only set for Mac OSX
